b8dd40cd90cb58d4c4f7c4b1dbedc38f327b7b2c
Dillon de Voor <dillon@crocodillon.com>
2016-04-04 20:47:14 +0200

    Update ECMAScript 2015 with some missing properties

--- bin/generate-readme.js
+++ bin/generate-readme.js
@@ -1,6 +1,14 @@
 const path = require('path')
 const fs   = require('fs')
 
+// Non-globals can’t be accessed directly from the global object
+const nonGlobals = [
+  '%TypedArray%',
+  'TypedArray',
+  'GeneratorFunction',
+  'Generator'
+]
+
 const specs = [
   require('../specifications/ECMAScript 1.json'),
   require('../specifications/ECMAScript 2.json'),
@@ -14,11 +22,14 @@ const tableHeader = `&nbsp;|${specs.map(spec =>
     `[${spec.shortname}](${spec.baseUrl})`
   ).join('|')}\n---${new Array(specs.length + 1).join('|---')}`
 const tableData =
-    specs[latest].sections.map(section =>
-      `|**\`${section.name}\`**|${
-        specs.map(spec => getSectionData(spec, section.name)).join('|')
-      }|`
-    ).join('\n')
+    specs[latest].sections.map(section => {
+      const root = section.name.split('.')[0]
+      const name = nonGlobals.indexOf(root) === -1 ?
+        `**\`${section.name}\`**` :
+        `*\`${section.name}\`*`
+      const data = specs.map(spec => getSectionData(spec, section.name))
+      return `|${name}|${data.join('|')}|`
+    }).join('\n')
 
 
 function getSectionData(spec, name) {
